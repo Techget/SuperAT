5b4cbc3f69064fea1ad8d555fc3da23b879f1514
xuan tong <txmanialife@gmail.com>
2022-12-19 12:27:56 +1100

    update attacker performance estimation

--- stage1_evaluate_attack_effect.py
+++ stage1_evaluate_attack_effect.py
@@ -11,34 +11,39 @@ from lightning_VAE import VAE
 # type (torch.cuda.FloatTensor) should be the same or input should be a MKLDNN tensor and weight is a dense 
 # tensor" when there is insufficient memory
 
+checkpoint_dir='./superAT_attacker_checkpoints/'
+model_state_file_name = 'stage1_attacker_epoch_32' # Rename to your trained model
 
 # Verify attacker effect
 # attack the same pre-trained model, compare the accuracy 
 def main():
-    x_test, y_test = load_cifar10(n_examples=2)
+    x_test, y_test = load_cifar10(n_examples=100) # use more examples
     # print(y_test) # => [3, 8, 8, 0, 6, 6, 1, 6, 3 ...]
-    model = load_model(model_name='Carmon2019Unlabeled', dataset='cifar10', threat_model='Linf')
+    model = load_model(model_name='Rebuffi2021Fixing_70_16_cutmix_extra', dataset='cifar10', threat_model='Linf')
 
-    # PGD attack the model
-    fmodel = fb.PyTorchModel(model, bounds=(0, 1))
-    _, advs, success = fb.attacks.LinfPGD()(fmodel, x_test.to('cuda:0'), y_test.to('cuda:0'), epsilons=[8/255])
-    print('Robust accuracy: {:.1%}'.format(1 - success.float().mean()))
+    # PGD attack the model, or refer to the autoattack robustaccuracy on the robustbench github repo
+    # fmodel = fb.PyTorchModel(model, bounds=(0, 1))
+    # _, advs, success = fb.attacks.LinfPGD()(fmodel, x_test.to('cuda:0'), y_test.to('cuda:0'), epsilons=[8/255])
+    # print('Robust accuracy: {:.1%}'.format(1 - success.float().mean()))
 
-    # Our trained attacker
-    checkpoint_dir='./superAT_attacker_checkpoints/'
+    # Our trained attacker attack the model
     attacker=VAE(input_height=32)
-    load_checkpoint(attacker, checkpoint_dir, 'attacker')
+    load_checkpoint(attacker, checkpoint_dir, model_state_file_name)
     attacker = attacker.eval()
     
-    x_rescontructed,_,_,_,_ = attacker.run_step(x_test)
-    model_output_for_x_reconstructed = model(x_rescontructed)
-    model_output = torch.argmax(model_output_for_x_reconstructed, 1)
     attacker_success = []
-    for i, mo in enumerate(model_output):
-        if mo == y_test[i]:
+    # loop 1 by 1 due to memory limit, can put in larger batch if more memory is available
+    for i, x_test_input in enumerate(x_test):
+        x_test_input = x_test_input[None, :, :,:] # convert shape from [3,32,32] to [1,3,32,32]
+        x_rescontructed,_,_,_,_ = attacker.run_step(x_test_input)
+        model_output_for_x_reconstructed = model(x_rescontructed)
+        model_output = torch.argmax(model_output_for_x_reconstructed, 1)
+        
+        if model_output[0] == y_test[i]:
             attacker_success.append(False)
         else:
             attacker_success.append(True)
+
     attacker_success = torch.BoolTensor(attacker_success)
     print('Our attacker Robust accuracy: {:.1%}'.format(1 - attacker_success.float().mean()))
 
